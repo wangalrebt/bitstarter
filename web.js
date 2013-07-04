b3d2c30ca5cac54b03dd74efff08ca73fc159693
wangalrebt <wangalrebt@gmail.com>
2013-07-04 15:45:55 -0400

    added index.html to web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var file = fs.readFileSync('index.html');
+var buffer = new Buffer(file);
+
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
